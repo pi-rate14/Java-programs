a7874e42113f84bba05d5467c0b72511411c346b
apoorva srivastava <apoorvasrivastava.14@gmail.com>
2021-05-21 20:15:02 +0530

    updated cyclesheet3 Q1 and Q5

--- Java-Cycle-Sheet-2/Q1.java
+++ Java-Cycle-Sheet-2/Q1.java
@@ -1,45 +1,129 @@
 package com.apoorva;
-import java.util.NoSuchElementException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Set;
 
-public class Q1{
+public class Q1 {
+    static Map<String, List<String>> h1 = new HashMap<String, List<String>>();
+    static Map<String, Integer> h2 = new HashMap<String, Integer>();
 
-    static void checkInput(String r, String n){
-        if(r.length() != 9){
-            System.out.println("Invalid");
-            throw new IllegalArgumentException("Register Number must contain exactly 9 characters");
-        }
-        if(n.length() != 10){
-            System.out.println("Invalid");
-            throw new IllegalArgumentException("Mobile Number must contain exactly 10 characters");
-        }
-
-        String pattern = "^[6|7|8|9]{1}\\d{9}";
-        Pattern a = Pattern.compile(pattern);
-        Matcher m1 = a.matcher(n);
-        if(!m1.find()){
-            throw new NumberFormatException("Mobile Number must contain only numeric values");
-        }
-
-        String pattern2 = "^[1-9]{2}[A-Z]{3}[0-9]{4}$";
-        Pattern b = Pattern.compile(pattern2);
-        Matcher m2 = b.matcher(r);
-        if(!m2.find()){
-            throw new NoSuchElementException("Registration Number must contain only digits and alphabets");
-        }
-
+    public void add_in_h1(String key, List<String> values) {
+        h1.put(key, values);
+        System.out.println(key + " added!");
     }
 
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Registration Number: ");
-        String reg = sc.nextLine();
-        System.out.println("Enter Phone Number: ");
-        String no = sc.nextLine();
-        sc.close();
-        checkInput(reg, no);
-        System.out.println("Valid");
+    public void delete_in_h1(String key) {
+        h1.remove(key);
+        System.out.println(key+" deleted!");
+    }
+
+    public void findFaculties(String key) {
+        if (h1.containsKey(key)) {
+            List<Integer> faculties = new ArrayList<Integer>();
+            List<String> list = h1.get(key);
+            Set<String> keys = h2.keySet();
+            for (String l : list) {
+                for (String k : keys) {
+                    if (l.equals(k)) {
+                        faculties.add(h2.get(k));
+                    }
+                }
+            }
+            System.out.println(faculties);
+        } else
+            System.out.println("No such student");
+    }
+
+    public static void main(String[] args) {
+        int choice = 0;
+        List<String> valSetOne = new ArrayList<String>();
+        valSetOne.add("Python");
+        valSetOne.add("Maths");
+        valSetOne.add("C");
+
+        List<String> valSetTwo = new ArrayList<String>();
+        valSetTwo.add("C");
+        valSetTwo.add("C++");
+
+        List<String> valSetThree = new ArrayList<String>();
+        valSetThree.add("C++");
+        valSetThree.add("Physics");
+        valSetThree.add("Chemistry");
+
+        h1.put("A", valSetOne);
+        h1.put("B", valSetTwo);
+        h1.put("C", valSetThree);
+
+        h2.put("Python", 111);
+        h2.put("Maths", 222);
+        h2.put("C", 333);
+        h2.put("C++", 444);
+        h2.put("Physics", 555);
+        h2.put("Chemistry", 666);
+        h2.put("Digital Electronics", 777);
+
+
+
+
+            Scanner sc = new Scanner(System.in);
+            do {
+                System.out.println("\n1)Add a student in H1.\n" +
+                        "2)Remove a student in H1.\n" +
+                        "3)Display both the maps\n" +
+                        "4)Find faculty of a student\n" +
+                        "5)Exit\n"
+                );
+                System.out.println("enter choice:");
+                choice = sc.nextInt();
+
+                switch (choice) {
+                    case 1:
+                        Q1 c2 = new Q1();
+                        System.out.println("enter name of student to add");
+                        String nameAdd = sc.next();
+                        List<String> val = new ArrayList<String>();
+                        val.add("Social Science");
+                        val.add("Computer Science");
+                        val.add("DSA");
+                        c2.add_in_h1(nameAdd, val);
+                        System.out.println("\n");
+                        break;
+                    case 2:
+                        Q1 c1 = new Q1();
+                        System.out.println("enter name of student to remove");
+                        String nameDelete = sc.next();
+                        c1.delete_in_h1(nameDelete);
+                        System.out.println("\n");
+                        break;
+                    case 3:
+                        System.out.println("Keys and corresponding Values in H1:");
+                        for (Map.Entry<String, List<String>> entry1 : h1.entrySet()) {
+                            String key1 = entry1.getKey();
+                            List<String> values1 = entry1.getValue();
+                            System.out.println(key1 + ":" + values1);
+                        }
+                        System.out.println("\n");
+                        System.out.println("Keys and corresponding Values in H2:");
+                        for (Map.Entry<String, Integer> entry2 : h2.entrySet()) {
+                            String key2 = entry2.getKey();
+                            Integer values2 = entry2.getValue();
+                            System.out.println(key2 + ":" + values2);
+                        }
+                        System.out.println("\n");
+                        break;
+                    case 4:
+                        Q1 c3 = new Q1();
+                        System.out.println("enter name of student to find faculties of");
+                        String nameFaculty = sc.next();
+                        c3.findFaculties(nameFaculty);
+                        System.out.println("\n");
+                        break;
+                    default:
+                        System.out.println("Thank you for using the program!");
+                }
+            } while (choice != 5);
     }
 }
--- Java-Cycle-Sheet-2/Q5.java
+++ Java-Cycle-Sheet-2/Q5.java
@@ -1,111 +1,98 @@
 package com.apoorva;
-
-import java.util.Scanner;
 import java.io.*;
-import java.time.LocalDate;
-import java.time.Period;
+import java.util.*;
 
-class Donor implements java.io.Serializable {
-    String name, address, blood_group, date_of_last_donation;
-    int age;
-    long contact_number;
-
-    Donor(String name, String address, String blood_group, String date_of_last_donation, int age, long contact_number) {
-        this.name = name;
-        this.address = address;
-        this.blood_group = blood_group;
-        this.date_of_last_donation = date_of_last_donation;
-        this.age = age;
-        this.contact_number = contact_number;
+class Sample implements Serializable {
+    public int pid;
+    public int diameter;
+    public int length;
+    public int weight;
+    public Sample(int pid, int diameter, int length, int weight) {
+        this.pid = pid;
+        this.diameter = diameter;
+        this.length = length;
+        this.weight = weight;
     }
 }
 
-public class Q5 {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number of objects: ");
-        int n = sc.nextInt();
-        sc.nextLine();
-        Donor objs[] = new Donor[n];
-        String filename = "DonorFile.ser";
-        System.out.println("Enter current date in format yyyy-mm-dd");
-        String currDate = sc.nextLine();
-        // Serialization
-        try {
-            // Saving of object in a file
-            FileOutputStream file = new FileOutputStream(filename);
-            ObjectOutputStream out = new ObjectOutputStream(file);
+class sortById implements Comparator<Sample>{
+    public int compare(Sample a, Sample b){
+        return a.pid - b.pid;
+    }
+}
 
-            for (int i = 0; i < n; i++) {
-                System.out.println("Enter name of donor " + (i + 1) + ": ");
-                String name = sc.nextLine();
-                System.out.println("Enter address of donor " + (i + 1) + ": ");
-                String address = sc.nextLine();
-                System.out.println("Enter blood group of donor " + (i + 1) + ": ");
-                String blood_group = sc.nextLine().toLowerCase();
-                System.out.println("Enter date of last donation of donor " + (i + 1) + " in format yyyy-mm-dd: ");
-                String date_of_last_donation = sc.nextLine();
-                System.out.println("Enter age of donor " + (i + 1) + ": ");
-                int age = sc.nextInt();
-                System.out.println("Enter contact number of donor " + (i + 1) + ": ");
-                long contact_number = sc.nextLong();
-                sc.nextLine();
-                objs[i] = new Donor(name, address, blood_group, date_of_last_donation, age, contact_number);
-                out.writeObject(objs[i]);
+public class Q5{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int pid;
+        int diameter;
+        int length;
+        int weight;
+        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> list1 = new ArrayList<>();
+        int i = 0;
+        int n;
+        System.out.print("Number of product samples : ");
+        n = scanner.nextInt();
+        for (int l = 0; l < n; l++) {
+            System.out.println("");
+            System.out.println("");
+            System.out.print("Enter the ID of product " + (l + 1) + " : ");
+            pid = scanner.nextInt();
+            System.out.print("Enter the Length(cm) of product " + (l + 1) + " : ");
+            length = scanner.nextInt();
+            System.out.print("Enter the Diameter(cm) of product " + (l + 1) + " : ");
+            diameter = scanner.nextInt();
+            System.out.print("Enter the Weight(gms) of product " + (l + 1) + " : ");
+            weight = scanner.nextInt();
+            Sample object = new Sample(pid, diameter, length, weight);
+            String filename = "file.txt";
+
+
+            try {
+
+                FileOutputStream file = new FileOutputStream(filename);
+                ObjectOutputStream out = new ObjectOutputStream(file);
+                out.writeObject(object);
             }
-            out.close();
-            file.close();
 
-            System.out.println("Object has been serialized");
+            catch (IOException ex) {
+                System.err.println(ex);
+            }
 
-        }
 
-        catch (IOException ex) {
-            System.out.println("IOException is caught");
-        }
-        Donor object1 = null;
+            Sample object2 = null;
+            try{
+                FileInputStream file = new FileInputStream(filename);
+                ObjectInputStream in = new ObjectInputStream(file);
 
-        // Deserialization
-        try {
-            boolean cont = true;
-            // Reading the object from a file
-            FileInputStream file = new FileInputStream(filename);
-            ObjectInputStream in = new ObjectInputStream(file);
-            int x = 0;
-            int count = 0;
-            while (cont == true && x < n) {
-                x++;
-                object1 = (Donor) in.readObject();
-                if (object1 != null) {
-                    System.out.println(object1.blood_group);
-                    System.out.println(object1.date_of_last_donation);
+                object2 = (Sample) in.readObject();
+                in.close();
+                file.close();
 
-                    if ((object1.blood_group.equals("a+"))
-                            && Period.between(LocalDate.parse(object1.date_of_last_donation), LocalDate.parse(currDate))
-                                    .getMonths() > 6) {
-                        System.out.println(object1.name + " " + object1.address + " " + object1.blood_group + " "
-                                + object1.date_of_last_donation + " " + object1.contact_number + " " + object1.age);
-                        count = 1;
-                    }
+                if (object2.length == 10 && object2.diameter == 3 && object2.weight == 100) {
+                    list1.add(String.valueOf(object2.pid));
                 }
 
+                if (object2.length != 10 || object2.diameter != 3 || object2.weight != 100) {
+                    list.add(String.valueOf(object2.pid));
+                }
             }
-            // Method for deserialization of object
-            if (count == 0)
-                System.out.println("No such donor");
+            catch (IOException ex) {
+                System.err.println(ex);
+            }
+            catch (ClassNotFoundException ex) {
+                System.out.println("ClassNotFoundException");
+            }
+        }
+        System.out.println("");
+        list.sort(list1, new sortById() );
+        System.out.println("Following are Non-Defective Product ID's : ");
 
-            in.close();
-            file.close();
+        for (String k : list1) {
+            System.out.println(k);
         }
 
-        catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        catch (ClassNotFoundException ex) {
-            System.out.println("ClassNotFoundException is caught");
-        }
 
     }
-
 }
